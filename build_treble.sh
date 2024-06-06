c0f40f074efa5765194fb312287aef157a572d2d
kaii <70664258+kaii-lb@users.noreply.github.com>
2024-06-06 18:57:50 +0300

    Update build_treble.sh

--- build_treble.sh
+++ build_treble.sh
@@ -5,19 +5,18 @@ rm -rf .repo/local_manifests
 
 repo init -u https://github.com/ProjectEverest/manifest -b qpr2 --git-lfs
 
-git clone https://github.com/ahnet-69/treble_evo .repo/local_manifests
+git clone https://github.com/kaii-lb/treble_evo_fucked .repo/local_manifests
 
 # mkdir device/phh/treble/
 # mv .repo/local_manifests/evo.mk device/phh/treble/evo.mk
 
-.repo/local_manifests/patches/apply-patches.sh . trebledroid
-# .repo/local_manifests/patches/apply-patches.sh . personal
-.repo/local_manifests/patches/apply-patches.sh . ponces
+.repo/local_manifests/patches/apply-patches.sh trebledroid
+# .repo/local_manifests/patches/apply-patches.sh personal
+.repo/local_manifests/patches/apply-patches.sh ponces
 
 # cd device/phh/treble
 # bash generate.sh evo
-
-cd ../../../ 
+#cd ../../../ 
 
 /opt/crave/resync.sh
 
