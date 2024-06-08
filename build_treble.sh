d4e3d410a42b9d0b0af5cf605b2d7b7e83822f04
kaii <imkaiilb@gmail.com>
2024-06-08 17:19:57 +0300

    reordered out of order commands

--- build_treble.sh
+++ build_treble.sh
@@ -7,29 +7,24 @@
 
 rm -rf .repo/local_manifests
 rm -rf treblestuff/
-rm -rf device/phh/treble/
-rm -rf .git/rebase-apply/
-rm -rf vendor/hardware_overlay/
+# rm -rf device/phh/treble/
+# rm -rf .git/rebase-apply/
+# rm -rf vendor/hardware_overlay/
 
 repo init -u https://github.com/ProjectEverest/manifest -b qpr2 --git-lfs
 
 git clone https://github.com/kaii-lb/treble_manifest.git .repo/local_manifests
-git clone https://github.com/TrebleDroid/vendor_hardware_overlay.git vendor/hardware_overlay/
+# git clone https://github.com/TrebleDroid/vendor_hardware_overlay.git vendor/hardware_overlay/
 git clone https://github.com/kaii-lb/treble_everest.git treblestuff/
 
-ls treblestuff/ 1>/dev/null
-if [ $? != 0 ]; then
-  echo "ERROR: syncing treble_everest failed."
-  exit 1
-fi
 
-cp treblestuff/everest.mk device/phh/treble/everest.mk
+# git clone https://github.com/TrebleDroid/device_phh_treble.git device/phh/treble/
 
-cd device/phh/treble
-git clean -fdx
-bash generate.sh everest
-cd ../../../ 
-echo "LOG: done generating."
+# ls device/phh/treble 1>/dev/null
+# if [ $? != 0 ]; then
+#   echo "ERROR: syncing device_phh_treble failed."
+#   exit 1
+# fi
 
 # /opt/crave/resync.sh
 date
@@ -56,13 +51,19 @@ if [ $? != 0 ]; then
   git rebase --abort
 fi
 
-git clone https://github.com/TrebleDroid/device_phh_treble.git device/phh/treble/
-
-ls device/phh/treble 1>/dev/null
+ls treblestuff/ 1>/dev/null
 if [ $? != 0 ]; then
-  echo "ERROR: syncing device_phh_treble failed."
+  echo "ERROR: syncing treble_everest failed."
   exit 1
 fi
+
+cp treblestuff/everest.mk device/phh/treble/everest.mk
+
+cd device/phh/treble
+git clean -fdx
+bash generate.sh everest
+cd ../../../ 
+echo "LOG: done generating."
 
 export EVEREST_MAINTAINER="kaii"
 export TARGET_SUPPORTS_BLUR=true
