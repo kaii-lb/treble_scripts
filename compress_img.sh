f3264a08de4ccf9b1f0a0367418f92b74b762e45
kaii <imkaiilb@gmail.com>
2024-07-27 14:15:12 +0300

    switching to xz compression

--- compress_img.sh
+++ compress_img.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /tmp/src/android/out/target/product/tdgsi_arm64_ab/
-tar c system.img | gzip --best > EverestOS_1_3_arm64_bgN.tar.gz
+#tar c system.img | gzip --best > EverestOS_1_3_arm64_bgN.tar.gz
+tar -c -I 'xz -9 -T0' -f EverestOS_1_3_arm64_bgN.tar.xz system.img
 cd /tmp/src/android/
-# gh release upload v1.0 --repo https://github.com/kaii-lb/treble_manifest.git out/target/product/tdgsi_arm64_ab/EverestOS_1_3_arm64_bgN.tar.gz
