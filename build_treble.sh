62c9a068fa238d8971663c00ae980a613e1ae6fd
kaii <imkaiilb@gmail.com>
2024-07-29 15:39:24 +0300

    added a check for failing make to exit script

--- build_treble.sh
+++ build_treble.sh
@@ -100,7 +100,11 @@ echo -e "--> done eating."
 # make systemimage -j $(nproc --all)
 make systemimage -j$(nproc --all)
 
+if [ $? != 0 ]; then
+  echo "--> ERROR: building treble_everest failed."
+  exit 1
+fi
+
 cd /tmp/src/android/out/target/product/tdgsi_arm64_ab/
-#tar c system.img | gzip --best > EverestOS_1_3_arm64_bgN.tar.gz
 tar -c -I 'xz -9 -T0' -f EverestOS_1_3_arm64_bgN.tar.xz system.img
 cd /tmp/src/android/
