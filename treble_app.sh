347c8b0953bc1d81ccfbc63597bdb78fa22ca5b0
kaii <imkaiilb@gmail.com>
2024-07-29 21:44:30 +0300

    disabling treble_app build out of fear

--- treble_app.sh
+++ treble_app.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+set -e
+
 echo "--> Building treble_app"
 cd treble_app
 
 # causes issues, exits entire script on success/failure
-if bash build.sh release; then
-	echo "--> SUCCESSFULLY BUILT TREBLE APP"
-else 
-	echo "--> BUILDING TREBLE APP FAILED"
-fi
+# if bash build.sh release; then
+# 	echo "--> SUCCESSFULLY BUILT TREBLE APP"
+# else 
+# 	echo "--> BUILDING TREBLE APP FAILED"
+# fi
 
 cp TrebleApp.apk ../vendor/hardware_overlay/TrebleApp/app.apk
 cd ../
