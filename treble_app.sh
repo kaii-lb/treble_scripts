7ac785852a6031e0e86019e0edad7ae7929072a9
kaii <imkaiilb@gmail.com>
2024-08-03 13:38:24 +0300

    reworked scripts again

--- treble_app.sh
+++ treble_app.sh
@@ -3,16 +3,12 @@
 set -e
 
 echo "--> Building treble_app"
-cd treble_app
-
-# causes issues, exits entire script on success/failure
-if bash build.sh release; then
-	echo "--> SUCCESSFULLY BUILT TREBLE APP"
-else 
-	echo "--> BUILDING TREBLE APP FAILED"
-fi
-
-cp TrebleApp.apk ../vendor/hardware_overlay/TrebleApp/app.apk
-cd ../
-echo "--> Finished building treble_app $PWD"
-echo
+pushd treble_app || exit
+	# causes issues, exits entire script on success/failure
+	if bash build.sh release; then
+		echo "--> SUCCESSFULLY BUILT TREBLE APP"
+	else 
+		echo "--> BUILDING TREBLE APP FAILED"
+	fi
+	cp TrebleApp.apk ../vendor/hardware_overlay/TrebleApp/app.apk
+popd || exit
