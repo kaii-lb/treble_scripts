69250691b395b1fbc417f6a065661a1495a2a4b9
kaii <imkaiilb@gmail.com>
2024-08-09 00:13:51 +0300

    a little touchup on treble_app

--- treble_app.sh
+++ treble_app.sh
@@ -4,10 +4,6 @@ echo "--> Building treble_app"
 cd /tmp/src/android/treble_app
 	# causes issues, exits entire script on success/failure
 	# can't exit script if not in script ;D
-	if bash build.sh release; then
-		echo "--> SUCCESSFULLY BUILT TREBLE APP"
-	else 
-		echo "--> BUILDING TREBLE APP FAILED"
-	fi
+	bash build.sh release
 	cp TrebleApp.apk ../vendor/hardware_overlay/TrebleApp/app.apk
 cd /tmp/src/android
