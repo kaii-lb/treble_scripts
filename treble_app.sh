e54069947b1748489c380a3578542f087442e9fb
kaii <imkaiilb@gmail.com>
2024-08-09 00:45:02 +0300

    reworked treble_app.sh

--- treble_app.sh
+++ treble_app.sh
@@ -4,6 +4,12 @@ echo "--> Building treble_app"
 cd /tmp/src/android/treble_app
 	# causes issues, exits entire script on success/failure
 	# can't exit script if not in script ;D
-	bash build.sh release
+	# bash build.sh release
+	wget https://github.com/kaii-lb/randomshit/releases/download/v0.1/TrebleApp.apk
+
+	if [ $? != 0 ]; then
+	  bash build.sh release
+	fi
+	
 	cp TrebleApp.apk ../vendor/hardware_overlay/TrebleApp/app.apk
 cd /tmp/src/android
