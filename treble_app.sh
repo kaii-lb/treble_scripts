8ca4ac66bae1c24cc2d7e0ea0f1bc013c743189f
kaii <imkaiilb@gmail.com>
2024-08-09 00:47:16 +0300

    reworked treble_app.sh

--- treble_app.sh
+++ treble_app.sh
@@ -8,7 +8,9 @@ cd /tmp/src/android/treble_app
 	wget https://github.com/kaii-lb/randomshit/releases/download/v0.1/TrebleApp.apk
 
 	if [ $? != 0 ]; then
-	  bash build.sh release
+	  	bash build.sh release
+	else
+		echo "using own TrebleApp.apk"
 	fi
 	
 	cp TrebleApp.apk ../vendor/hardware_overlay/TrebleApp/app.apk
