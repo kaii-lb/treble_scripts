e49e5281830577e3ba5ca9b0e937e99e9e9a88eb
kaii <imkaiilb@gmail.com>
2024-08-29 15:53:37 +0300

    trying without aosp clang

--- prepare.sh
+++ prepare.sh
@@ -66,8 +66,8 @@ echo -e "--> Starting resync at $(date)."
 /opt/crave/resync.sh
 echo -e "--> Resync done at $(date)."
 
-rm -r prebuilts/clang/host/linux-x86
-git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 prebuilts/clang/host/linux-x86
+# rm -r prebuilts/clang/host/linux-x86
+# git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 prebuilts/clang/host/linux-x86
 
 rm -rf vendor/lineage/signing/keys
 git clone https://github.com/kaii-lb/everestos_keys.git vendor/lineage/signing/keys && echo && echo "Added personal signing keys"
